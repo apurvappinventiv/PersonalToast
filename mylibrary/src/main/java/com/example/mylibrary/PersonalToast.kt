d937661be5eed7ab55a149245dc019e429982d72
Apoorv Singh <apoorva.singh@appinventiv.com>
2024-01-29 16:34:32 +0530

    Personal Toast

--- mylibrary/src/main/java/com/example/mylibrary/PersonalToast.kt
+++ mylibrary/src/main/java/com/example/mylibrary/PersonalToast.kt
@@ -1,10 +1,15 @@
 package com.example.mylibrary
 
+import android.app.Activity
 import android.content.Context
 import android.widget.Toast
 
 class PersonalToast {
     fun Context.ShowPersonalToast(msz: String) {
+        Toast.makeText(this, msz, Toast.LENGTH_LONG).show()
+    }
+
+    fun Activity.ShowPersonalToast(msz: String) {
         Toast.makeText(this, msz, Toast.LENGTH_LONG).show()
     }
 }
