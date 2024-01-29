a641544e6c443ae9ceaa10b1006fcef3b7f43157
Apoorv Singh <apoorva.singh@appinventiv.com>
2024-01-29 16:55:24 +0530

    Personal Toast

--- app/src/main/java/com/example/personaltoast/MainActivity.kt
+++ app/src/main/java/com/example/personaltoast/MainActivity.kt
@@ -10,15 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mylibrary.PersonalToast
 import com.example.personaltoast.ui.theme.PersonalToastTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             PersonalToastTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
--- mylibrary/src/main/java/com/example/mylibrary/PersonalToast.kt
+++ mylibrary/src/main/java/com/example/mylibrary/PersonalToast.kt
@@ -3,13 +3,14 @@ package com.example.mylibrary
 import android.app.Activity
 import android.content.Context
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class PersonalToast {
-    fun Context.ShowPersonalToast(msz: String) {
+class PersonalToast(var context: Context) {
+    fun AppCompatActivity.ShowPersonalToast(msz: String) {
         Toast.makeText(this, msz, Toast.LENGTH_LONG).show()
     }
 
-    fun Activity.ShowPersonalToast(msz: String) {
-        Toast.makeText(this, msz, Toast.LENGTH_LONG).show()
+    fun ShowPersonalToast(msz: String) {
+        Toast.makeText(context, msz, Toast.LENGTH_LONG).show()
     }
 }
